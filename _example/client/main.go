a8926cbe85a46ba25efc013000cc24d95ea6809f
miaoyin <miaoyin@woda.com>
2022-11-25 17:45:08 +0800

    test: example client

--- _example/client/main.go
+++ _example/client/main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"sync"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	group := sync.WaitGroup{}
 	group.Add(100)
 
 	for i := 0; i < 100; i++ {
-		go func() {
-			http.Get("http://127.0.0.1:8080")
+		go func(num int) {
+			res, _ := http.Get("http://127.0.0.1:8080")
+
+			fmt.Printf("%02d => %d, %v\n", num, res.StatusCode, time.Since(start))
 			group.Done()
-		}()
+		}(i)
 	}
 
 	group.Wait()
