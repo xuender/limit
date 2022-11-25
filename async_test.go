6ef0fc79a5d3e2ce429fd8e974a2b5a5ac85f20f
miaoyin <miaoyin@woda.com>
2022-11-25 17:30:55 +0800

    test: Close

--- async_test.go
+++ async_test.go
@@ -11,35 +11,39 @@ import (
 func TestNewLimiter(t *testing.T) {
 	t.Parallel()
 
-	lim := limit.NewAsync(1, time.Second, func(num int) { time.Sleep(time.Microsecond) })
 	ass := assert.New(t)
+	limiter := limit.NewAsync(1, time.Second, func(num int) { time.Sleep(time.Microsecond) })
 
-	ass.Nil(lim.Add(1))
-	ass.Nil(lim.Add(1))
-	ass.Equal(1, lim.Len())
-	ass.NotNil(lim.Add(1))
+	ass.Nil(limiter.Add(1))
+	ass.Nil(limiter.Add(1))
+	ass.Equal(1, limiter.Len())
+	ass.NotNil(limiter.Add(1))
+	limiter.Close()
 
-	lim = limit.NewAsync(1, time.Millisecond, func(num int) { time.Sleep(time.Microsecond) })
+	limiter = limit.NewAsync(1, time.Millisecond, func(num int) { time.Sleep(time.Microsecond) })
 
-	ass.Nil(lim.Add(1))
-	ass.Nil(lim.Add(1))
-	ass.NotNil(lim.Add(1))
+	ass.Nil(limiter.Add(1))
+	ass.Nil(limiter.Add(1))
+	ass.NotNil(limiter.Add(1))
+	limiter.Close()
 
-	lim = limit.NewAsync(1000, time.Millisecond, func(num int) { time.Sleep(time.Microsecond) })
+	limiter = limit.NewAsync(1000, time.Millisecond, func(num int) { time.Sleep(time.Microsecond) })
 
-	ass.Nil(lim.Add(1))
-	ass.Nil(lim.Add(1))
+	ass.Nil(limiter.Add(1))
+	ass.Nil(limiter.Add(1))
 	time.Sleep(time.Millisecond * 2)
-	ass.Nil(lim.Add(1))
-	lim.Close()
+	ass.Nil(limiter.Add(1))
+	limiter.Close()
 }
 
 func TestLimiterAdd(t *testing.T) {
 	t.Parallel()
 
-	lim := limit.NewAsync(0, time.Second, func(num int) { time.Sleep(time.Microsecond) })
+	ass := assert.New(t)
+	limiter := limit.NewAsync(0, time.Second, func(num int) { time.Sleep(time.Microsecond) })
 
-	assert.Nil(t, lim.Add(1))
-	assert.Nil(t, lim.Add(1))
-	assert.Nil(t, lim.Add(1))
+	ass.Nil(limiter.Add(1))
+	ass.Nil(limiter.Add(1))
+	ass.Nil(limiter.Add(1))
+	limiter.Close()
 }
